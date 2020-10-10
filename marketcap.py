27d48867188815c34c54f8c05bea28a4d476b654
marriamichail <61513587+marriamichail@users.noreply.github.com>
2020-10-10 18:06:30 +0300

    Add files via upload
    This is the code that I 've made to download market caps for different stock tickers loaded from an excel that I have.

---
+++ marketcap.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+from openpyxl import load_workbook
+wb = load_workbook('/Users/maria/Job/Python/Ws.xlsx')  # Work Book
+ws = wb['Sheet2']  # Work Sheet
+column = ws['C']  # Column
+tickers_list = [column[x].value for x in range(len(column))]
+
+print(*tickers_list, sep='\n')  #επιβεβαιώνω ότι λειτουργεί σωστά και τυπώνει τα tickers από excel
+print(len(tickers_list))  #επιβεβαιώνω ότι το μέγεθος των στοιχείων είναι σωστό
+
+#extract the marketcaps
+import pandas_datareader as web
+tickers = tickers_list[1:]
+print(tickers)
+market_cap_data= web.get_quote_yahoo(tickers)['marketCap']
+print(market_cap_data)
+
